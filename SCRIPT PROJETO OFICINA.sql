7bf6652273304082278999d3ec3440ec6b2a0e83
unknown <silvanemota@outlook.com>
2021-05-17 18:30:52 -0300

    Atualizacao com Modelagem no Banco de Dados

--- SCRIPT PROJETO OFICINA.sql
+++ SCRIPT PROJETO OFICINA.sql
@@ -187,18 +187,146 @@ INSERT INTO COR VALUES (NULL, 'Marron');
 INSERT INTO COR VALUES (NULL, 'Laranja');
 INSERT INTO COR VALUES (NULL, 'Azul');
 
-SELECT COR FROM COR;
-+--------------+
-| COR          |
-+--------------+
-| Amarelo      |
-| Azul         |
-| Branco       |
-| Cinza Chumbo |
-| Laranja      |
-| Marron       |
-| Perolado     |
-| Prata        |
-| Preto        |
-| Vermelho     |
-+--------------+
+ SELECT COR, IDCOR FROM COR;
++--------------+-------+
+| COR          | IDCOR |
++--------------+-------+
+| Amarelo      |     3 |
+| Azul         |    10 |
+| Branco       |     7 |
+| Cinza Chumbo |     5 |
+| Laranja      |     9 |
+| Marron       |     8 |
+| Perolado     |     6 |
+| Prata        |     2 |
+| Preto        |     1 |
+| Vermelho     |     4 |
++--------------+-------+
+INSERT INTO CARRO_COR VALUES (1,2);
+INSERT INTO CARRO_COR VALUES (1,3);
+INSERT INTO CARRO_COR VALUES (2,3);
+INSERT INTO CARRO_COR VALUES (3,10);
+INSERT INTO CARRO_COR VALUES (3,6);
+INSERT INTO CARRO_COR VALUES (4,1);
+INSERT INTO CARRO_COR VALUES (5,4);
+INSERT INTO CARRO_COR VALUES (6,9);
+INSERT INTO CARRO_COR VALUES (6,8);
+INSERT INTO CARRO_COR VALUES (7,7);
+INSERT INTO CARRO_COR VALUES (8,5);
+INSERT INTO CARRO_COR VALUES (8,9);
+INSERT INTO CARRO_COR VALUES (9,8);
+INSERT INTO CARRO_COR VALUES (10,1);
+INSERT INTO CARRO_COR VALUES (11,10);
+INSERT INTO CARRO_COR VALUES (11,6);
+INSERT INTO CARRO_COR VALUES (12,5);
+
+SELECT * FROM CARRO_COR;
++----------+--------+
+| ID_CARRO | ID_COR |
++----------+--------+
+|        4 |      1 |
+|       10 |      1 |
+|        1 |      2 |
+|        1 |      3 |
+|        2 |      3 |
+|        5 |      4 |
+|        8 |      5 |
+|       12 |      5 |
+|        3 |      6 |
+|       11 |      6 |
+|        7 |      7 |
+|        6 |      8 |
+|        9 |      8 |
+|        6 |      9 |
+|        8 |      9 |
+|        3 |     10 |
+|       11 |     10 |
++----------+--------+
+
+SHOW CREATE TABLE CARRO_COR;
+
+SELECT CLIENTE.NOME, TELEFONE.TIPO, TELEFONE.NUMERO, CLIENTE.SEXO, CARRO.MODELO, MARCA.MARCA, CARRO.PLACA, COR.COR
+FROM CARRO
+INNER JOIN CLIENTE
+ON IDCARRO = CLIENTE.ID_CARRO
+INNER JOIN TELEFONE
+ON CLIENTE.IDCLIENTE = TELEFONE.ID_CLIENTE
+INNER JOIN MARCA
+ON CARRO.ID_MARCA = MARCA.IDMARCA
+INNER JOIN CARRO_COR
+ON IDCARRO = CARRO_COR.ID_CARRO
+INNER JOIN COR
+ON COR.IDCOR = CARRO_COR.ID_COR;
++------------------------------+------+----------------+------+-----------------------+-----------+---------+--------------+
+| NOME                         | TIPO | NUMERO         | SEXO | MODELO                | MARCA     | PLACA   | COR          |
++------------------------------+------+----------------+------+-----------------------+-----------+---------+--------------+
+| Elisa Melo Batista           | RES  | 11 2467-0987   | F    | Onix                  | Chevrolet | OKR8R13 | Vermelho     |
+| Anna Cristina da Silva       | CEL  | 11 90765-2345  | F    | C4 Lounge             | Citroen   | FDS6J78 | Branco       |
+| Claudia Simone R. da Mota    | RES  | 11 4567-1233   | F    | Aircross              | Citroen   | XCV0M45 | Cinza Chumbo |
+| Claudia Simone R. da Mota    | RES  | 11 4567-1233   | F    | Aircross              | Citroen   | XCV0M45 | Laranja      |
+| Guilherme Henrique Gonçalves | RES  | 11 4765-7865   | M    | Cronos                | Fiat      | EWS1F89 | Preto        |
+| Silvane Melo da Mota         | CEL  | 11 93456-9876  | F    | EcoSport              | Ford      | LKM6Z78 | Marron       |
+| Silvane Melo da Mota         | RES  | 11 3423-8976   | F    | EcoSport              | Ford      | LKM6Z78 | Marron       |
+| Rosangela Alqueja            | CEL  | 11 95643-0976  | F    | Ranger (Cabine Dupla) | Ford      | SLY9X56 | Preto        |
+| Nelson Rodrigues da Mota     | CEL  | 11 995643-0912 | M    | Sportage              | Kia       | PQD9F67 | Perolado     |
+| Nelson Rodrigues da Mota     | CEL  | 11 995643-0912 | M    | Sportage              | Kia       | PQD9F67 | Azul         |
+| Nelson Rodrigues da Mota     | CEL  | 11 98532-5554  | M    | Sportage              | Kia       | PQD9F67 | Perolado     |
+| Nelson Rodrigues da Mota     | CEL  | 11 98532-5554  | M    | Sportage              | Kia       | PQD9F67 | Azul         |
+| Gilcelli Melo da Mota        | RES  | 11 4234-4567   | F    | Sorento               | Kia       | RFB0L76 | Cinza Chumbo |
++------------------------------+------+----------------+------+-----------------------+-----------+---------+--------------+
+
+SELECT MODELO, COUNT(*) AS 'QUANT.'
+FROM CARRO
+GROUP BY MODELO;
++-----------------------+--------+
+| MODELO                | QUANT. |
++-----------------------+--------+
+| Aircross              |      1 |
+| C4 Lounge             |      1 |
+| City Ex CVT           |      1 |
+| Cronos                |      1 |
+| Cruze                 |      1 |
+| EcoSport              |      1 |
+| Fit LX CVT            |      1 |
+| Onix                  |      1 |
+| Ranger (Cabine Dupla) |      1 |
+| Sorento               |      1 |
+| Sportage              |      1 |
+| Toro                  |      1 |
++-----------------------+--------+
+
+SELECT SEXO, COUNT(*) AS 'QUANT'
+FROM CLIENTE
+GROUP BY SEXO;
++------+-------+
+| SEXO | QUANT |
++------+-------+
+| M    |     4 |
+| F    |     8 |
++------+-------+
+
+SELECT CLIENTE.NOME, CLIENTE.SEXO, TELEFONE.TIPO, TELEFONE.NUMERO, CARRO.MODELO, MARCA.MARCA, COR.COR
+FROM CLIENTE
+INNER JOIN TELEFONE
+ON IDCLIENTE = TELEFONE.ID_CLIENTE
+INNER JOIN CARRO
+ON CLIENTE.ID_CARRO = CARRO.IDCARRO
+INNER JOIN MARCA
+ON CARRO.ID_MARCA = MARCA.IDMARCA
+INNER JOIN CARRO_COR
+ON CARRO_COR.ID_CARRO = CARRO.IDCARRO
+INNER JOIN COR
+ON COR.IDCOR = CARRO_COR.ID_COR
+WHERE SEXO = 'F';
++---------------------------+------+------+---------------+-----------------------+-----------+--------------+
+| NOME                      | SEXO | TIPO | NUMERO        | MODELO                | MARCA     | COR          |
++---------------------------+------+------+---------------+-----------------------+-----------+--------------+
+| Elisa Melo Batista        | F    | RES  | 11 2467-0987  | Onix                  | Chevrolet | Vermelho     |
+| Anna Cristina da Silva    | F    | CEL  | 11 90765-2345 | C4 Lounge             | Citroen   | Branco       |
+| Claudia Simone R. da Mota | F    | RES  | 11 4567-1233  | Aircross              | Citroen   | Cinza Chumbo |
+| Claudia Simone R. da Mota | F    | RES  | 11 4567-1233  | Aircross              | Citroen   | Laranja      |
+| Silvane Melo da Mota      | F    | CEL  | 11 93456-9876 | EcoSport              | Ford      | Marron       |
+| Silvane Melo da Mota      | F    | RES  | 11 3423-8976  | EcoSport              | Ford      | Marron       |
+| Rosangela Alqueja         | F    | CEL  | 11 95643-0976 | Ranger (Cabine Dupla) | Ford      | Preto        |
+| Gilcelli Melo da Mota     | F    | RES  | 11 4234-4567  | Sorento               | Kia       | Cinza Chumbo |
++---------------------------+------+------+---------------+-----------------------+-----------+--------------+
